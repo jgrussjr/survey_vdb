97337b40dad78bde7fe675870a9ffeff8b8ad16b
James Gruss, Jr <jgrussjr@gmail.com>
2015-12-15 22:34:42 -0500

    read me file iframe

---
+++ infopage.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="css/mystyle.css" />
+		<link rel="stylesheet" href="css/normalize.css" />
+		<link rel="stylesheet" href="css/customstyle.css" />
+		<script src="scripts/jquery-1.8.2.min.js"></script>
+	</head>
+<body>
+<?php
+require_once('navbar.html');
+?>
+<div class="outerframe">
+	<div class="myframe">
+		<iframe class="myframe" src="README.md"></iframe>
+	</div>
+</div>
+</body>
+</html>
